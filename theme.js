7802d34074be2574b482b1370fedf4da821e331d
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-11-04 22:53:02 -0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,11 +1,5 @@
 var e = document.getElementsByTagName('html')[0];
-const att = document.createAttribute("style");
-att.value = "--leftSidebarColor:#300d20; --pageColor:#ffffff00; --contentColor:#290b1b; --contentColor2:#2e0c1e; --contentColor3:#380f25; --borderColor:#42122c; --fontColor:#ffffff; --themeColor:#FF42A7;";
-var d = document.createElement('html');
-
-d.innerHTML = e.innerHTML;
-d.setAttributeNode(att);
-e.parentNode.replaceChild(d, e);
+e.style = "--leftSidebarColor:#300d20; --pageColor:#17060f80; --contentColor:#290b1b; --contentColor2:#2e0c1e; --contentColor3:#380f25; --borderColor:#42122c; --fontColor:#ffffff; --themeColor:#FF42A7;";
 
 function add(tag, str) {
 // Create element:
@@ -14,7 +8,7 @@ para.innerHTML = str;
 
 // Append to body:
 document.body.appendChild(para);
-alert('Added element. TagName: ' + tag + "innerHTML: " + str + "");
 }
 
-let p = add("style", `body { background-color: #95234a; animation-name: example; animation-duration: 4s; animation-iteration-count: 1.7976931348623157e+309; } @keyframes example { 0%   {background-color: #95234a;} 25%  {background-color: #4b87bc;} 50%  {background-color: #1e183b;} 75% {background-color: #3f7ca9;} 100%   {background-color: #95234a;} }`);
+let p = add("style", `body { background-color: #95234a; animation-name: example; animation-duration: 4s; animation-iteration-count: 1.7976931348623157e+309; } @keyframes example { 0%   {background-color: #95234a;} 25%  {background-color: #4b87bc;} 50%  {background-color: #1e183b;} 75% {background-color: #3f7ca9;} 100%   {background-color: #95234a;} } .pageHolder { background-color: var(--pageColor); border-radius: 12px; } .stickyContainer { position: sticky; top: 8px; z-index: 500; border-radius: 12px; box-shadow: 0px 0px 0px var(--pageColor), 0px 0px 0px var(--pageColor); } .sidebar { border-radius: 12px; position: sticky; display: flex; flex-direction: column; box-sizing: border-box; width: 200px; height: calc(100vh - 16px); top: 8px; background: var(--pageColor); color: var(--themeColor); font-size: 20px; line-height: 20px; font-family: var(--secondFont); font-weight: bold; }`);
+
